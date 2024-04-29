6dbb4dcf0bbb5ef73c81b81d77fdd52d50cda767
las13816 <las13816@email.vccs.edu>
2024-04-28 23:01:08 -0400

    created extDateType

---
+++ extDateType.h
@@ -0,0 +1,28 @@
+// Header guard 
+#ifndef extDateType_H
+#define extDateType_H
+#include <string>
+// Include dateType header 
+#include "dateType.h"
+
+// Declare class as a branch from dateType 
+class extDateType : public dateType {
+public:
+    //Static array 
+    static std::string eMonths[13];
+
+    void printLongDate();
+    void setDate(int, int, int);
+    void setMonth(int m);
+
+    void printLongMonthYear();
+    // Constructors 
+    extDateType();
+    extDateType(int, int, int);
+    // Private instance variable
+private:
+    // Instance variable that stores the month 
+    std::string eMonth;
+};
+
+#endif
